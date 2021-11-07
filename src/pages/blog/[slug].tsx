363f0aa0bebe67a4bf7b60f251ff361024a281b0
adalbertopc <adalbertinho90@gmail.com>
2021-11-06 20:37:06 -0700

    feat: add blog base files

---
+++ src/pages/blog/[slug].tsx
@@ -0,0 +1,11 @@
+import { Layout } from '../../components/Layout';
+
+export default function Article() {
+  return (
+    <Layout>
+      <div>
+        <h1>Article</h1>
+      </div>
+    </Layout>
+  );
+}
---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,13 @@
+import { Layout } from '../../components/Layout';
+
+export default function Blog() {
+  return (
+    <Layout>
+      <h1>Blog</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
+        quisquam.
+      </p>
+    </Layout>
+  );
+}
