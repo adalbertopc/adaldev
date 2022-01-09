bb6c0f57e7199ac5739515c7349130f7e6eeca2e
adalbertopc <adalbertinho90@gmail.com>
2022-01-08 18:26:35 -0700

    feat: add projects

--- src/components/projects/ProjectCard.tsx
+++ src/components/projects/ProjectCard.tsx
@@ -1,5 +1,8 @@
 import Image from 'next/image';
+import { CodeIcon, EyeIcon } from '@heroicons/react/solid';
 import { IProject } from '../../lib/interfaces/Project';
+import { Button } from '../Button';
+import { useState } from 'react';
 
 export const ProjectCard = ({
   title,
@@ -11,14 +14,35 @@ export const ProjectCard = ({
   const imageUrl = `${
     image[0].url.startsWith('/') ? process.env.STRAPI_URI : ''
   }${image[0].url}`;
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <article>
-      <div className="relative aspect-w-16 aspect-h-9">
-        <Image src={imageUrl} layout="fill" objectFit="cover" />
+    <article className="rounded-3xl shadow-md p-4 relative transition-all transform hover:scale-105">
+      <div className="relative aspect-w-16 aspect-h-9 rounded-2xl">
+        <Image
+          src={imageUrl}
+          layout="fill"
+          objectFit="cover"
+          className="rounded-2xl"
+          placeholder="blur"
+          blurDataURL={imageUrl}
+        />
       </div>
       <div>
-        <h2>{title}</h2>
-        <p>{description}</p>
+        <h2 className="my-3 font-headings font-bold text-lg">{title}</h2>
+        <p className={`${!isOpen && 'line-clamp-3'} text-sm`}>{description}</p>
+        <button
+          className="font-semibold text-sm"
+          onClick={() => setIsOpen(!isOpen)}>
+          {isOpen ? 'Read Less' : 'Read More'}
+        </button>
+        <div className="mt-4 flex gap-2">
+          <Button>
+            <EyeIcon className="w-4 h-4" />
+          </Button>
+          <Button>
+            <CodeIcon className="w-4 h-4" />
+          </Button>
+        </div>
       </div>
     </article>
   );
--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -7,19 +7,23 @@ interface ProjectsProps {
 
 export const Projects = ({ projects }: ProjectsProps) => {
   return (
-    <section>
-      <h2>Projects</h2>
-      {projects.map(({ id, title, description, image, repo, preview }) => (
-        <ProjectCard
-          key={id}
-          id={id}
-          title={title}
-          description={description}
-          image={image}
-          repo={repo}
-          preview={preview}
-        />
-      ))}
+    <section className="my-12 md:my-20">
+      <h2 className="font-headings font-bold text-center text-2xl md:text-3xl md:text-left mb-6">
+        Featured Projects
+      </h2>
+      <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        {projects.map(({ id, title, description, image, repo, preview }) => (
+          <ProjectCard
+            key={id}
+            id={id}
+            title={title}
+            description={description}
+            image={image}
+            repo={repo}
+            preview={preview}
+          />
+        ))}
+      </div>
     </section>
   );
 };
