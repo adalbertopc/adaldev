b2ba0eeed5986988e3bd457f8cf7843f8a3ef177
adalbertopc <adalbertinho90@gmail.com>
2022-01-09 02:50:52 -0700

    fix: add classname to div

--- src/components/InnerLayout.tsx
+++ src/components/InnerLayout.tsx
@@ -5,6 +5,9 @@ interface InnerLayoutProps {
 
 export const InnerLayout = ({ children, className }: InnerLayoutProps) => {
   return (
-    <div className={`container max-w-6xl px-4 mx-auto md:px-6`}>{children}</div>
+    <div
+      className={`container max-w-6xl px-4 mx-auto md:px-6 ${className || ''}`}>
+      {children}
+    </div>
   );
 };
